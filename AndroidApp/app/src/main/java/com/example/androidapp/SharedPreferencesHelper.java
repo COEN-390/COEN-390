b2ce47a2ab6219ff896cd9dce41eb0900dd3d7ac
DeathSpreads <68755892+DeathSpreads@users.noreply.github.com>
2021-10-29 14:19:35 -0400

    Solved connection problem to the server
    There is still a bug where the SharedPreferencesHelper cannot store the username. Sometimes, it looks as if the entire account.createSession() is skipped for no reason (debugger doesn't stop at breakpoints inside of it).

--- AndroidApp/app/src/main/java/com/example/androidapp/SharedPreferencesHelper.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/SharedPreferencesHelper.java
@@ -11,6 +11,10 @@ import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
+
 import io.appwrite.Client;
 import io.appwrite.exceptions.AppwriteException;
 import io.appwrite.services.Account;
@@ -35,10 +39,16 @@ public class SharedPreferencesHelper {
         this.sharedPreferences = context.getSharedPreferences(this.context.getString(R.string.Shared_Preferences), Context.MODE_PRIVATE);
         this.editor = sharedPreferences.edit();
 
+        // Initialize the Appwrite communication
         this.client = new Client(context)
                 .setEndpoint("https://appwrite.orpine.net/v1")
                 .setProject("6137a2ef0d4f5");
         this.account = new Account(this.client);
+
+        // Initialize the user
+        this._user = new MutableLiveData<JSONObject>();
+        this._user.setValue(null);
+        this.user = this._user;
     }
 
     public String getUsername(){
@@ -68,6 +78,11 @@ public class SharedPreferencesHelper {
                                 } else {
                                     storeUsername();
                                 }
+                            } catch (AppwriteException e){
+                                System.out.println("createSession() " + new Timestamp(System.currentTimeMillis()));
+                                System.out.println(e.getMessage());
+                                System.out.println(e.getCode());
+                                System.out.println(e.getResponse());
                             } catch (Throwable th) {
                                 Log.e("ERROR", "Unable to create session");
                             }
@@ -75,23 +90,17 @@ public class SharedPreferencesHelper {
                     }
             );
         } catch (AppwriteException e) {
-            e.printStackTrace();
-        }
-        // Store the username
-        this._user = new MutableLiveData<JSONObject>();
-        this._user.setValue(null);
-        this.user = this._user;
-        try {
-            storeUsername();
-        } catch (AppwriteException e) {
-            e.printStackTrace();
-        }
+            System.out.println("createSession() " + new Timestamp(System.currentTimeMillis()));
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+            System.out.println(e.getResponse());
+         }
     }
 
     public void endSession(){
         try {
             account.deleteSession(
-                    "[SESSION_ID]",
+                    "current",
                     new Continuation<Object>() {
                         @NotNull
                         @Override
@@ -110,6 +119,11 @@ public class SharedPreferencesHelper {
                                     Response response = (Response) o;
                                     json = response.body().string();
                                 }
+                            } catch (AppwriteException e){
+                                System.out.println("endSession() " + new Timestamp(System.currentTimeMillis()));
+                                System.out.println(e.getMessage());
+                                System.out.println(e.getCode());
+                                System.out.println(e.getResponse());
                             } catch (Throwable th) {
                                 Log.e("ERROR", "Unable to end session");
                             }
@@ -117,40 +131,56 @@ public class SharedPreferencesHelper {
                     }
             );
         } catch (AppwriteException e) {
-            e.printStackTrace();
+            System.out.println("endSession() " + new Timestamp(System.currentTimeMillis()));
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+            System.out.println(e.getResponse());
         }
         this.editor.putString(this.context.getString(R.string.Shared_Preferences_Username), "");
+        this._user.setValue(null);
     }
 
-    private void storeUsername() throws AppwriteException{
-        account.get(
-            new Continuation<Object>() {
-                @NotNull
-                @Override
-                public CoroutineContext getContext() {
-                    return EmptyCoroutineContext.INSTANCE;
-                }
-
-                @Override
-                public void resumeWith(@NotNull Object o) {
-                    String json = "";
-                    try {
-                        if (o instanceof Result.Failure) {
-                            Result.Failure failure = (Result.Failure) o;
-                            throw failure.exception;
-                        } else {
-                            Response response = (Response) o;
-                            json = response.body().string();
-                            JSONObject user = new JSONObject(json);
-                            _user.postValue(user);
+    private void storeUsername() {
+        try {
+            account.get(
+                    new Continuation<Object>() {
+                        @NotNull
+                        @Override
+                        public CoroutineContext getContext() {
+                            return EmptyCoroutineContext.INSTANCE;
                         }
-                    } catch (Throwable th) {
-                        Log.e("ERROR", "Unable to get user");
+
+                        @Override
+                        public void resumeWith(@NotNull Object o) {
+                            String json = "";
+                            try {
+                                if (o instanceof Result.Failure) {
+                                    Result.Failure failure = (Result.Failure) o;
+                                    throw failure.exception;
+                                } else {
+                                    Response response = (Response) o;
+                                    json = response.body().string();
+                                    JSONObject user = new JSONObject(json);
+                                    _user.postValue(user);
+                                }
+                            } catch (AppwriteException e) {
+                                System.out.println("storeUsername() " + new Timestamp(System.currentTimeMillis()));
+                                System.out.println(e.getMessage());
+                                System.out.println(e.getCode());
+                                System.out.println(e.getResponse());
+                            } catch (Throwable th) {
+                                Log.e("ERROR", "Unable to get user");
+                            }
+                        }
                     }
-                }
-            }
-        );
-        if(user.getValue() != null) {
+            );
+        } catch (AppwriteException e){
+            System.out.println("storeUsername() " + new Timestamp(System.currentTimeMillis()));
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+            System.out.println(e.getResponse());
+        }
+        if(user.getValue().equals(null)) {
             try {
                 this.editor.putString(this.context.getString(R.string.Shared_Preferences_Username), user.getValue().getString("name"));
             } catch (JSONException e) {
