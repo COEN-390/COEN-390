688e728edfcd44ef8dbd07455556d925e56318f7
Kevin <promkev@gmail.com>
2021-11-07 20:39:55 -0500

    Added event data

--- FCM-CloudFunction/Program.cs
+++ FCM-CloudFunction/Program.cs
@@ -1,7 +1,9 @@
-﻿using FirebaseAdmin;
+﻿using System;
+using System.Threading.Tasks;
+using FirebaseAdmin;
 using FirebaseAdmin.Messaging;
 using Google.Apis.Auth.OAuth2;
-using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace FCMPushNotifications
 {
@@ -9,11 +11,17 @@ namespace FCMPushNotifications
     {
         static async Task Main(string[] args)
         {
+            // Contains the JSON passed to the cloud function
+            var eventData = Environment.GetEnvironmentVariable("APPWRITE_FUNCTION_EVENT_DATA");
+            dynamic eventDataJson = JObject.Parse(eventData);
+
+            // Grabs the GOOGLE_APPLICATION_CREDENTIALS from the environment variable to authenticate to FCM
             var messaging = FirebaseMessaging.GetMessaging(FirebaseApp.Create(new AppOptions()
             {
                 Credential = GoogleCredential.GetApplicationDefault(),
             }));
 
+            // Sends the notification
             await messaging.SendAsync(new Message()
             {
                 Notification = new Notification()
